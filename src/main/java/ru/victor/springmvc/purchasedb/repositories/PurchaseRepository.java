0b983ef948365a437e14c2a3aff767643d94f366
Liar123 <krasn20161@mail.ru>
2024-04-05 04:13:11 +0300

    сделал 2 простых запрса на внесение чтение данных из бд

---
+++ src/main/java/ru/victor/springmvc/purchasedb/repositories/PurchaseRepository.java
@@ -0,0 +1,33 @@
+package ru.victor.springmvc.purchasedb.repositories;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.victor.springmvc.purchasedb.PurchaseDbApplication;
+import ru.victor.springmvc.purchasedb.model.Purchase;
+
+import java.util.List;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbcTemplate;
+    public PurchaseRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;//мы будем обращаться к этому объекту класса для какг либо взаимодействия с бд
+
+    }
+    public void storePurchase(Purchase purchase){
+        String sql = "INSERT INTO purchases VALUES(NULL,?,?)";
+        jdbcTemplate.update(sql,purchase.getProduct(),purchase.getPrice());
+    }
+        public List<Purchase> findAllPurchases(){
+         String sql = "SELECt * FROM purchases";
+         RowMapper<Purchase> rowMapper = (r,i) -> {
+             Purchase rowObject = new Purchase();
+             rowObject.setId(r.getInt("id"));
+             rowObject.setProduct(r.getString("product"));
+             rowObject.setPrice(r.getBigDecimal("price"));
+             return rowObject;//даннорое лямбда выражение создает для каждой строки sql ответа объекта типа purchase
+         };
+         return jdbcTemplate.query(sql,rowMapper);
+        }
+}
---
+++ src/test/java/ru/victor/springmvc/purchasedb/PurchaseDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.victor.springmvc.purchasedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
